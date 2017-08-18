fa29579816bd6ffd9b332508845ebc98145fd53d
Jack Pincus <jwpincus@gmail.com>
2017-08-18 11:09:08 -0700

    take the safety off- removes git head check

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,14 +49,14 @@ end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/vanilla_deploy`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
+  # task :check_revision do
+  #   on roles(:app) do
+  #     unless `git rev-parse HEAD` == `git rev-parse origin/vanilla_deploy`
+  #       puts "WARNING: HEAD is not the same as origin/master"
+  #       puts "Run `git push` to sync changes."
+  #       exit
+  #     end
+  #   end
   end
 
   desc 'Initial Deploy'
